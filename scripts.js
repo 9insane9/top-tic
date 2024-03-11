0e266000674b4cf15d21d3be08c436c2458f0b40
9insane9 <145199591+9insane9@users.noreply.github.com>
2024-03-11 15:48:16 +0200

    needs rework

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const gameBoard = (function () {
-    const board = [ "","","",
+    let board = [ "","","",
                     "","","",
                     "","","",
                     ];
@@ -8,12 +8,42 @@ const gameBoard = (function () {
 
 }());
 
-const game = (function () {
-    const players = playerManager()
+const playerManager = (function () {
+    const players = [];
+
+    function createPlayer (name) {
+        if (players.length < 2) {
+            const player = { name };
+            players.push(player);
+        } else {
+            console.log("already have two players")
+        }
+        assignMarkers(players)
+    }
+
+    function assignMarkers (playersArray) {
+        if (players.length === 2) {
+            playersArray.forEach(function(player, index) {
+                index === 0 ? player.marker = "X" : player.marker = "O"
+        })}}
+
+    function getPlayers() {
+        return players;
+    }
+
     console.log(players)
+
+    return {getPlayers, createPlayer};
+}())
+
+
+const game = (function () {
+    const players = playerManager.getPlayers();
     const firstPlayer = 0;
     const secondPlayer = 1;
     let gameOver = false;
+    let gameWon = false;
+    let gameTied = false;
 
     let activePlayer = firstPlayer;
 
@@ -29,7 +59,7 @@ const game = (function () {
             console.log(gameBoard.board)
 
             findWinner(gameBoard.board);
-            isGameOver()
+            isGameTied()
 
             setActivePlayer()
 
@@ -38,9 +68,10 @@ const game = (function () {
             }
     }
 
-    function isGameOver () {
-        if (!gameBoard.board.includes("") || gameOver) {
-            console.log("game is over")
+    function isGameTied () {
+        if (!gameBoard.board.includes("") && (gameOver && !gameWon)) {
+            console.log("game is tied")
+            gameTied = true;
         }
     }
 
@@ -57,11 +88,14 @@ const game = (function () {
         ) {
             console.log(`player${activePlayer} has won!`)
             gameOver = true;
+            gameWon = true;
+        } else if (!gameBoard.board.includes("")) {
+            console.log("game tied")
         }
     }
 
     function setActivePlayer () {
-        if (!gameOver) {
+        if (!gameOver && gameBoard.board.includes("")) {
             activePlayer === firstPlayer ? activePlayer = secondPlayer : activePlayer = firstPlayer;
             console.log(`next up is player${activePlayer}`)
         } else {
@@ -69,40 +103,27 @@ const game = (function () {
         }
     }
 
-    return {placeMarker}
+    function restart () {
+        gameOver = false;
+        gameWon = false;
+        gameTied = false;
+        activePlayer = firstPlayer;
+        gameBoard.board = [ "","","",
+                            "","","",
+                            "","","", ];
+    }
+
+    return {placeMarker, restart}
 
 }())
 
-function playerManager() {
-    const players = [];
 
-    function assignMarkers (playersArray) {
-        if (players.length === 2) {
-            playersArray.forEach(function(player, index) {
-                index === 0 ? player.marker = "X" : player.marker = "O"
-        })}    
-    }
-
-    function createPlayer (name) {
-        if (players.length < 2) {
-            const player = { name };
-            players.push(player);
-        } else {
-            console.log("already have two players")
-        }
-        assignMarkers(players)
-        
-    }
-
-    createPlayer("bib")
-    createPlayer("bob")
-
-    console.log(players)
-
-    return players;
-}
+playerManager.createPlayer("bib")
+playerManager.createPlayer("bob")
 
 
+
+////player0-win
 game.placeMarker(0)
 game.placeMarker(3)
 game.placeMarker(2)
@@ -112,3 +133,14 @@ game.placeMarker(4)
 game.placeMarker(7)
 game.placeMarker(6)
 game.placeMarker(8)
+
+// ////tie
+// game.placeMarker(0) 
+// game.placeMarker(1)
+// game.placeMarker(2)
+// game.placeMarker(4)
+// game.placeMarker(3)
+// game.placeMarker(5)
+// game.placeMarker(7)
+// game.placeMarker(6)
+// game.placeMarker(8)
